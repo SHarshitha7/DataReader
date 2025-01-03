f3ab527ccb8b8a0197701ce39b6435d4e7a5d8ff
Salavadi Harshitha <harshisalavadi@gmail.com>
2025-01-03 15:55:48 +0530

    Updated DataReader to simulate market data and publish to Kafka

--- data_reader.py
+++ data_reader.py
@@ -3,8 +3,10 @@ import time
 import json
 
 # Redis Configuration
-REDIS_HOST = "redis.finvedic.in"
-REDIS_PORT = 6379
+#REDIS_HOST = "redis.finvedic.in"
+#REDIS_PORT = 6379
+REDIS_HOST = "localhost"  
+REDIS_PORT = 6379 
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
